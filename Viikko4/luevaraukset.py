b5d0d23fbc95be1feffada60ca4ffbac155afecf
Juho Tiihonen <juho.tiihonen@centria.fi>
2025-11-21 19:03:06 +0200

    viimeistelty

--- Viikko4/luevaraukset.py
+++ Viikko4/luevaraukset.py
@@ -1,25 +1,5 @@
-"""
-Ohjelma joka tulostaa tiedostosta luettujen varausten alkiot ja niiden tietotyypit
-
-varausId | nimi | sähköposti | puhelin | varauksenPvm | varauksenKlo | varauksenKesto | hinta | varausVahvistettu | varattuTila | varausLuotu
-------------------------------------------------------------------------
-201 | Muumi Muumilaakso | muumi@valkoinenlaakso.org | 0509876543 | 2025-11-12 | 09:00:00 | 2 | 18.50 | True | Metsätila 1 | 2025-08-12 14:33:20
-int | str | str | str | date | time | int | float | bool | str | datetime
-------------------------------------------------------------------------
-202 | Niiskuneiti Muumilaakso | niisku@muumiglam.fi | 0451122334 | 2025-12-01 | 11:30:00 | 1 | 12.00 | False | Kukkahuone | 2025-09-03 09:12:48
-int | str | str | str | date | time | int | float | bool | str | datetime
-------------------------------------------------------------------------
-203 | Pikku Myy Myrsky | myy@pikkuraivo.net | 0415566778 | 2025-10-22 | 15:45:00 | 3 | 27.90 | True | Punainen Huone | 2025-07-29 18:05:11
-int | str | str | str | date | time | int | float | bool | str | datetime
-------------------------------------------------------------------------
-204 | Nipsu Rahapulainen | nipsu@rahahuolet.me | 0442233445 | 2025-09-18 | 13:00:00 | 4 | 39.95 | False | Varastotila N | 2025-08-01 10:59:02
-int | str | str | str | date | time | int | float | bool | str | datetime
-------------------------------------------------------------------------
-205 | Hemuli Kasvikerääjä | hemuli@kasvikeraily.club | 0463344556 | 2025-11-05 | 08:15:00 | 2 | 19.95 | True | Kasvitutkimuslabra | 2025-10-09 16:41:55
-int | str | str | str | date | time | int | float | bool | str | datetime
-------------------------------------------------------------------------
-"""
 from datetime import datetime
+
 
 def muunna_varaustiedot(varaus: list) -> list:
     # Tähän tulee siis varaus oletustietotyypeillä (str)
@@ -40,17 +20,20 @@ def muunna_varaustiedot(varaus: list) -> list:
     muutettu_varaus.append(datetime.strptime(varaus[10], "%Y-%m-%d %H:%M:%S"))
     return muutettu_varaus
 
+
 def hae_varaukset(varaustiedosto: str) -> list:
     # HUOM! Tälle funktioille ei tarvitse tehdä mitään!
     # Jos muutat, kommentoi miksi muutit
     varaukset = []
-    varaukset.append(["varausId", "nimi", "sähköposti", "puhelin", "varauksenPvm", "varauksenKlo", "varauksenKesto", "hinta", "varausVahvistettu", "varattuTila", "varausLuotu"])
+    varaukset.append(["varausId", "nimi", "sähköposti", "puhelin", "varauksenPvm", "varauksenKlo",
+                     "varauksenKesto", "hinta", "varausVahvistettu", "varattuTila", "varausLuotu"])
     with open(varaustiedosto, "r", encoding="utf-8") as f:
         for varaus in f:
             varaus = varaus.strip()
             varaustiedot = varaus.split('|')
             varaukset.append(muunna_varaustiedot(varaustiedot))
     return varaukset
+
 
 def main():
     # HUOM! seuraaville riveille ei tarvitse tehdä mitään osassa A!
@@ -68,13 +51,14 @@ def main():
     print("------------------------------------------------------")
     print("2) Pitkät varaukset(kesto vähintään 3 tuntia  )")
     for pitvaraus in varaukset[1:]:
-        if pitvaraus[6] >= 3:  
+        if pitvaraus[6] >= 3:
             nimi = pitvaraus[1]
             pvm = pitvaraus[4].strftime("%d.%m.%Y")
             kelloaika = pitvaraus[5].strftime("%H.%M")
             kesto = pitvaraus[6]
             tila = pitvaraus[9]
-            print(f"- {nimi}, Päivämäärä: {pvm} , kellonaika: {kelloaika} , kesto: {kesto} tuntia, tila: {tila}")
+            print(
+                f"- {nimi}, Päivämäärä: {pvm} , kellonaika: {kelloaika} , kesto: {kesto} tuntia, tila: {tila}")
     print("------------------------------------------------------")
 
     print("3) Varausten vahvistus status:")
@@ -93,13 +77,17 @@ def main():
     eivahvistetut_maara = sum(1 for varaus in varaukset[1:] if not varaus[8])
     print(f"- Vahvistettuja varauksia: {vahvistetut_maara}")
     print(f"- Ei vahvistettuja varauksia: {eivahvistetut_maara}")
-    
+
     print("------------------------------------------------------")
 
     print("5) Vahvistettujen varausten kokonaistulot")
     kokonaistulot = sum(varaus[7] for varaus in varaukset[1:] if varaus[8])
-    muutettu_kokonaistulot = f"{kokonaistulot:.2f}".replace('.', ',')   
-    print(f"- Vahvistettujen varausten kokonaistulot: {muutettu_kokonaistulot} euroa")
+    muutettu_kokonaistulot = f"{kokonaistulot:.2f}".replace('.', ',')
+    print(
+        f"- Vahvistettujen varausten kokonaistulot: {muutettu_kokonaistulot} euroa")
+    
+    print("------------------------------------------------------")
+
 
 if __name__ == "__main__":
     main()
