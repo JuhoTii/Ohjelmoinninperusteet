8cf879167383100d5d4a972ff1dbac132d5229ea
Juho Tiihonen <juho.tiihonen@centria.fi>
2025-11-30 16:11:11 +0200

    Lisatty main funktio

--- Viikko5/viikko5.py
+++ Viikko5/viikko5.py
@@ -3,9 +3,23 @@ import csv
 
 #Tiedoston polku
 file_path = 'viikko42.csv'
-
+def lue_csv(viikko5_csv: str) -> list:
 #luetaan tiedosto ja tulostetaan rivit
-with open(file_path,newline='', encoding='utf-8') as csvfile:
+
+ with open(file_path,newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile, delimiter=';')
     for row in reader:
         print(row)
+
+
+
+def main() -> None:
+    """Ohjelman pääfunktio: lukee datan, laskee yhteenvedot ja tulostaa raportin."""
+    ...
+
+
+
+
+
+    if __name__ == "__main__":
+        main()
